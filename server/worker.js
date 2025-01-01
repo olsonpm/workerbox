4ef245417f9c0e62a0670f4eeee3ce9e5365e8e4
Phil Olson <olsonpm@users.noreply.github.com>
2025-01-01 11:15:20 -0600

    use eval

--- server/worker.js
+++ server/worker.js
@@ -1,30 +1,6 @@
 import createCallbackStore from '../lib/createCallbackStore.js';
 import createSuperJSON from '../lib/createSuperJSON.js';
 
-async function scopedEval (context, expr) {
-  const evaluator = Function.apply(null, [
-    ...Object.keys(context),
-    expr
-  ]);
-  return evaluator.apply(null, [...Object.values(context)]);
-}
-
-const getStack = (error, slice) => {
-  const lines = error.stack.split('\n');
-  const stack = [
-    lines[0],
-    ...lines
-      .filter(line => line.includes('(eval at scopedEval'))
-      .map(line => {
-        const splitted = line.split('(eval at scopedEval (');
-        const [, mixedPosition] = line.split('<anonymous>');
-        const [, lineNumber, charNumber] = mixedPosition.slice(0, -1).split(':');
-        return `${splitted[0]}(<sandbox>:${lineNumber - 3}:${charNumber})`
-      })
-  ].slice(0, slice).join('\n');
-  return stack;
-}
-
 self.addEventListener('message', async (event) => {
   const port = event.ports[0];
 
@@ -43,16 +19,10 @@ self.addEventListener('message', async (event) => {
       const parsedScope = superjson.parse(scope);
 
       try {
-        const result = await scopedEval(parsedScope, code);
-
-        port.postMessage(['return', { id, args: superjson.stringify([result]) }]);
+        globalThis.workerboxScope = parsedScope
+        eval?.(code)
       } catch (error) {
-        try {
-          const stack = getStack(error, -1);
-          port.postMessage(['error', { id: errorId, args: superjson.stringify([stack || error.message]) }]);
-        } catch (error2) {
-          port.postMessage(['error', { id: errorId, args: superjson.stringify([error.message]) }]);
-        }
+        port.postMessage(['error', { id: errorId, args: superjson.stringify([error]) }]);
       }
     }
 
@@ -67,8 +37,7 @@ self.addEventListener('message', async (event) => {
         const result = await fn(...parsedArgs);
         port.postMessage(['return', { id: resolve, args: superjson.stringify([result]) }]);
       } catch (error) {
-        const stack = getStack(error);
-        port.postMessage(['error', { id: reject, args: superjson.stringify([stack || error.message]) }]);
+        port.postMessage(['error', { id: reject, args: superjson.stringify([error]) }]);
       }
     }
   };
